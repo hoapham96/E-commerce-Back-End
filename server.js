c695f91c2adcf6c313fb1069230e597ac05adeda
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-02-14 21:34:38 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const routes = require('./routes');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
